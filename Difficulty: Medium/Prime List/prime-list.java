a3c461e50b7a44a3f63e5f66599fc94807e11a90
Vaishu2916 <22951a12b0@iare.ac.in>
2025-05-04 15:14:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Prime List/prime-list.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Node {
+    Node next;
+    int val;
+
+    public Node(int data) {
+        val = data;
+        next = null;
+    }
+}
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t;
+        t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+
+            Node head, tail;
+            String s[] = in.readLine().trim().split(" ");
+            int num = Integer.parseInt(s[0]);
+            head = new Node(num);
+            tail = head;
+            for (int i = 1; i < s.length; i++) {
+                num = Integer.parseInt(s[i]);
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            Node temp = ob.primeList(head);
+            while (temp != null) {
+                out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            out.println();
+            out.println("~");
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+class Node{
+    Node next;
+    int val;
+    public Node(int data){
+        val=data;
+        next=null;
+    }
+}
+*/
+
+class Solution {
+    Node primeList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            curr.val = nearestPrime(curr.val);
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    int nearestPrime(int n) {
+        if (n <= 2) return 2;
+        int low = n, high = n;
+        while (!isPrime(low)) low--;
+        while (!isPrime(high)) high++;
+        return (n - low <= high - n) ? low : high;
+    }
+
+    boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+}
